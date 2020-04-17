98b80f2466670284fba0b69321bf3faf8cba470d
maannnne <mane.poghosian@gmail.com>
2020-04-17 12:02:08 +0400

    Add more registration tests

--- automation/test/pageObjects/register.page.js
+++ automation/test/pageObjects/register.page.js
@@ -5,10 +5,69 @@ module.exports['default'] = class Register extends Base{
     }
 
     //selectors as getter functions
+    get regPageHeader() {return $('[name="regPageHeader"]')};
     get newUserEmail() {return $('[name="newUserEmail"]');}
+    get newUserName() { return $('[name="newUserName"]')};
+    get newUserPass() {return $('[name="newUserPassword"]')};
+    get newUserConfPass() {return $('[name="confirmPassword"]')};
+    get registerButton() {return $('[name="registerButton"]')};
+    get languageButton() {return $('[name="regLangButton"]')};
+    get loginLink() {return $('[name="loginLink"]')};
+    get haveAccMessage() {return $('[name="haveAccMessage"]')};
+    get langArm() {return $('[class="dropdown-item arm-lang"]')};
+    get langEng() {return $('[class="dropdown-item eng-lang"]')};
 
+
+    getRegPageHeader() {
+        return browser.$(this.regPageHeader).getText();
+    };
+    
     setEmail(email){
         this.newUserEmail.waitForExist();
         this.newUserEmail.setValue(email);
+    };
+
+    setName(name){
+        this.newUserName.waitForExist();
+        this.newUserName.setValue(name);
+    };
+
+    setPass(pass){
+        this.newUserName.waitForExist();
+        this.newUserName.setValue(pass);
+    };
+
+    setConfPass(confPass){
+        this.newUserName.waitForExist();
+        this.newUserName.setValue(confPass);
+    };
+
+    clickOnRegisterButton(){
+        this.registerButton.waitForExist();
+        this.registerButton.click();
+    };
+
+    clickOnLanguageButton(){
+        this.languageButton.waitForExist();
+        this.languageButton.click();
+    };
+
+    setLangToArm(){
+        this.langArm.waitForExist();
+        this.langArm.click();
+    };
+
+    setLangToEng(){
+        this.langEng.waitForExist();
+        this.langEng.click();
+    };
+
+    clickOnLoginLink(){
+        this.loginLink.waitForExist();
+        this.loginLink.click();
     }
+
+
+    
+
 }
--- automation/test/specs/register.test.js
+++ automation/test/specs/register.test.js
@@ -2,18 +2,28 @@ const assert = require('assert');
 const RegisterPage  = require('../pageObjects/register.page').default;
 
 describe('kidsUp page', () => {
-    const testEmail = 'test@testttttttttttttttttttttttttttt';
     const registerPageObject = new RegisterPage();
 
-
-    
     it('should have the correct title - R-1', () => {
         browser.url('/register');
         const title = browser.getTitle();
         assert.equal(title, 'Register');
     });
 
-    it('should find and fill the email', () => {
-        registerPageObject.setEmail(testEmail);
+    it('should successfully change the language to Armenian R-2', () => {
+        // registerPageObject.setEmail(testEmail);
+        registerPageObject.clickOnLanguageButton();
+        registerPageObject.setLangToArm();
+        const regPageHeaderArm = registerPageObject.getRegPageHeader();
+        assert.equal(regPageHeaderArm, 'KidsUp! Ստեղծի՛ր քո նոր հաշիվը');
+        
+    });
+
+    it('should successfully change the language to English R-2', () => {
+        registerPageObject.clickOnLanguageButton();
+        registerPageObject.setLangToEng();
+        const regPageHeaderEng = registerPageObject.getRegPageHeader();
+        assert.equal(regPageHeaderEng, 'Create your brand new profile in KidsUp!');
     });
 });
+
